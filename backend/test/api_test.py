1c36877442cc2dab336b2fa013b5608586f7e511
ineyon <ineyoff@gmail.com>
2026-04-22 22:25:01 +0300

    main import fix

--- backend/test/api_test.py
+++ backend/test/api_test.py
@@ -1,12 +1,17 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 client = TestClient(app)
 
 def test_root():
     response = client.get("/")
-    assert response.statues_code == 200
-    assert response.json() == {"message" : "Godot CI Platform API"}
+    assert response.status_code == 200
+    assert response.json() == {"message": "Godot CI Platform API"}
 
 def test_health():
     response = client.get("/api/health")
