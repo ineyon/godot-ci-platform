d42598eaeb3b659184beb519ecef7a03866ec642
ineyon <ineyoff@gmail.com>
2026-05-11 20:36:35 +0300

    created github service and endpoint handling to this service

---
+++ backend/github_service.py
@@ -0,0 +1,30 @@
+from github import Github
+
+def get_builds(github_token: str, github_repo: str):
+    g = Github(github_token)
+    repo = g.get_repo(github_repo)
+    
+    runs = repo.get_workflow_runs() #отримуємо 10 останніх workflow з гіта
+    
+    result = []
+    for run in list(runs)[:10]:
+        result.append({
+            "id": run.id,
+            "name": run.name,
+            "status": run.status,
+            "conclusion": run.conclusion,
+            "created_at": str(run.created_at),
+            "commit": run.head_commit.message,
+            "url": run.html_url
+        })
+    
+    return result
+
+def trigger_build(github_token: str, github_repo: str): #примусово запускаємо воркфлоу
+    g = Github(github_token) 
+    repo = g.get_repo(github_repo)
+    
+    workflow = repo.get_workflow("build.yml")
+    workflow.create_dispatch(ref="main")
+    
+    return {"message": "Build triggered!"}
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware #для реакта безпека
 from models import Project # наш проектік
 from storage import load_projects, add_project, get_project, delete_project # функції для роботи з стореджом
+from github_service import get_builds, trigger_build
 
 app = FastAPI() # создаєм екземпляр фаст апішки
 
@@ -24,21 +25,35 @@ def health():
 def get_projects():
     return load_projects()
 
-@app.post("/api/projects")
+@app.post("/api/projects")  # додаємо проект
 def create_project(project: Project):
     return add_project(project.model_dump())
 
-@app.get("/api/projects/{project_id}")
+@app.get("/api/projects/{project_id}") # отримати один проект
 def get_one_project(project_id: str):
     project = get_project(project_id)
     if not project:
         raise HTTPException(status_code=404, detail="Project not found")
     return project
 
-@app.delete("/api/projects/{project_id}")
+@app.delete("/api/projects/{project_id}") # видаляємо проект
 def remove_project(project_id: str):
     project = get_project(project_id)
     if not project:
         raise HTTPException(status_code=404, detail="Project not found")
     delete_project(project_id)
     return {"message": "Project deleted"}
+
+@app.get("/api/projects/{project_id}/builds") #отримуємо 10 останніх білдів проекту так як в github service їх кількість стоїть 10 (треба буде глянуть чи цього не буде замало, якщо що то зробити більшим список)
+def get_project_builds(project_id: str):
+    project = get_project(project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return get_builds(project["github_token"], project["github_repo"])
+
+@app.post("/api/projects/{project_id}/trigger-build") #трігерим білд по мейну
+def trigger_project_build(project_id : str):
+    project = get_project(project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return trigger_build(project["github_token"], project["github_repo"])
